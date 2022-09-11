79a9fdc8e91cb682c28821d936b7200c246289e1
Tewary01 <tiamiyuteslim46@gmail.com>
2022-09-11 09:43:18 -0700

    Task 6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Prints sizes of characters
- *
- *   *
+ * main - Prints sizes of characters
  *
  *    * Return: Always 0 (success)
- *
  */
 int main(void)
 {	printf("Size of a char: %zu byte(s)\n", sizeof(char));
