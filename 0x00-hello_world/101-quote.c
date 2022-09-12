9536fb9237463d3e5e09d01df8ef3d66aad1f136
Tewary01 <tiamiyuteslim46@gmail.com>
2022-09-12 09:03:30 -0700

    101 -quote fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,17 @@
-{#include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - prints to string
  *
- * Description: Prints "and that piece of art is useful..." without puts
+ *  * main - Entry point
  *
- * Return: 1
-*/
+ *  *
+ *
+ *  * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long l = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-	__asm__ ("syscall"
-			: "=a" (ret)
-			: "a" (syscall),
-			"D" (fd),
-			"S" (s),
-			"d" (1));
 	return (1);
-}}
+}
